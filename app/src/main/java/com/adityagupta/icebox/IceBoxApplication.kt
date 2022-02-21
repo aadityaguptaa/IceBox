c6ef2e8b5fc0c2ad5c542880fad9d693f47f759d
Aditya Gupta <52538173+aadityaguptaa@users.noreply.github.com>
2022-02-21 05:59:23 +0530

    initial setup

---
+++ app/src/main/java/com/adityagupta/icebox/IceBoxApplication.kt
@@ -0,0 +1,8 @@
+package com.adityagupta.icebox
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class IceBoxApplication : Application(){
+}
---
+++ app/src/main/java/com/adityagupta/icebox/common/Constants.kt
@@ -0,0 +1,6 @@
+package com.adityagupta.icebox.common
+
+object Constants {
+
+    const val BASE_URL = ""
+}
