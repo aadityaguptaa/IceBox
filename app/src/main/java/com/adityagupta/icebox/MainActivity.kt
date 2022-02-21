2182446d68ad695f5393446d45097aba4edd5522
Aditya Gupta <52538173+aadityaguptaa@users.noreply.github.com>
2022-02-21 07:03:23 +0530

    started working on the home screen layout

--- app/src/main/java/com/adityagupta/icebox/MainActivity.kt
+++ app/src/main/java/com/adityagupta/icebox/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.adityagupta.icebox
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.adityagupta.icebox.databinding.ActivityMainBinding
+import com.adityagupta.icebox.presentation.homescreen.HomeScreenActivity
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.homeScreenButton.setOnClickListener {
+            startActivity(Intent(this, HomeScreenActivity::class.java))
+        }
+
     }
 }
---
+++ app/src/main/java/com/adityagupta/icebox/presentation/homescreen/HomeScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.adityagupta.icebox.presentation.homescreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.adityagupta.icebox.R
+
+class HomeScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_screen)
+    }
+}
